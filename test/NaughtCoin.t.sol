4a8b15681b794ee2c07dad6c37ee0152b963c054
StErMi <stermi@gmail.com>
2022-07-27 08:03:13 +0200

    Added additional assertion to check if the test worked as intended

--- test/NaughtCoin.t.sol
+++ test/NaughtCoin.t.sol
@@ -70,5 +70,6 @@ contract TestNaughtCoin is BaseTest {
         vm.stopPrank();
 
         assertEq(level.balanceOf(player), 0);
+        assertEq(level.balanceOf(tempUser), playerBalance);
     }
 }
